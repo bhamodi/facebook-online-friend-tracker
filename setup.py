4a2c5f2852bafae32c5939863039cb30e5b60147
Baraa Hamodi <baraa@optimizely.com>
2015-07-07 23:10:01 -0700

    Fixed README read issue.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='facebook-online-friend-tracker',
-  version='1.0.6',
+  version='1.0.7',
   description='This tool tracks the number of online friends a user has on Facebook at any given time.',
   long_description=open('README.rst').read(),
   url='https://github.com/bhamodi/facebook-online-friend-tracker',
